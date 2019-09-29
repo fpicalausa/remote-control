a8526f25c27f444a2c3e7795c9ffdfebaab1276c
Francois Picalausa <fpicalausa@gmail.com>
2019-09-29 20:17:02 +0900

    Make better transitions

--- read_temp.py
+++ read_temp.py
@@ -9,7 +9,7 @@ except:
 
 GPIO14 = 14
 
-SERIAL_START_LO = 18000000
+SERIAL_START_HOLD = 18000000
 SERIAL_RESPONSE_WAIT = 4000
 SERIAL_RESPONSE_MARK = 8000
 SERIAL_BIT_MARK = 5000
@@ -18,12 +18,14 @@ SERIAL_ZERO_MARK = 7000
 SERIAL_NO_MORE_DATA = 10000
 
 STATE_INIT = 0
-STATE_WAIT_RESPONSE = 1
-STATE_RESPONSE_LO = 2
-STATE_RESPONSE_HI = 3
-STATE_BIT_START = 4
-STATE_BIT_DATA = 5
-STATE_END = 6
+STATE_SEND_START = 1
+STATE_WAIT_RESPONSE = 2
+STATE_RESPONSE_LO = 3
+STATE_RESPONSE_HI = 4
+STATE_BIT_START = 5
+STATE_BIT_DATA = 6
+STATE_END = 7
+
 
 class SingleWireSerialTransport:
     def __init__(self, pi, pin):
@@ -34,13 +36,13 @@ class SingleWireSerialTransport:
         self.pi.set_pull_up_down(self.pin, pigpio.PUD_UP)
         self.pi.set_mode(self.pin, pigpio.INPUT)
 
-        #skip any communication we might have triggered
+        # skip any communication we might have triggered
         time.sleep(0.1)
 
     def _transition(self, target, current_state, next_state, last_transition, timeout):
         value = self.pi.read(self.pin)
         if value == target:
-            state = next_state
+            return next_state
         elif time.time_ns() - last_transition > timeout * 20:
             raise IOError('Timed out in state ' + str(current_state))
 
@@ -50,7 +52,7 @@ class SingleWireSerialTransport:
         state = STATE_INIT
         old_state = STATE_END
         last_transition = 0
-        bits_lengths = []
+        bits_length = []
 
         while state != STATE_END:
             if old_state != state:
@@ -61,31 +63,36 @@ class SingleWireSerialTransport:
             if state == STATE_INIT:
                 self.pi.set_mode(self.pin, pigpio.OUTPUT)
                 self.pi.write(self.pin, pigpio.LOW)
-                if (time.time_ns() - last_transition > SERIAL_START_LO):
+                state = STATE_SEND_START
+            elif state == STATE_SEND_START:
+                if (time.time_ns() - last_transition > SERIAL_START_HOLD):
+                    self.pi.set_mode(self.pin, pigpio.INPUT)
                     state = STATE_WAIT_RESPONSE
-                self.pi.write(self.pin, pigpio.HIGH)
-                self.pi.set_mode(self.pin, pigpio.INPUT)
             elif state == STATE_WAIT_RESPONSE:
-                state = self._transition(0, state, STATE_RESPONSE_LO, last_transition, SERIAL_RESPONSE_WAIT)
+                state = self._transition(
+                    0, state, STATE_RESPONSE_LO, last_transition, SERIAL_RESPONSE_WAIT)
             elif state == STATE_RESPONSE_LO:
-                state = self._transition(1, state, STATE_RESPONSE_HI, last_transition, SERIAL_RESPONSE_MARK)
+                state = self._transition(
+                    1, state, STATE_RESPONSE_HI, last_transition, SERIAL_RESPONSE_MARK)
             elif state == STATE_RESPONSE_LO:
-                state = self._transition(0, state, STATE_BIT_START, last_transition, SERIAL_RESPONSE_MARK)
+                state = self._transition(
+                    0, state, STATE_BIT_START, last_transition, SERIAL_RESPONSE_MARK)
             elif state == STATE_BIT_START:
-                state = self._transition(1, state, STATE_BIT_DATA, last_transition, SERIAL_BIT_MARK)
+                state = self._transition(
+                    1, state, STATE_BIT_DATA, last_transition, SERIAL_BIT_MARK)
             elif state == STATE_BIT_DATA:
                 value = self.pi.read(self.pin)
 
                 if value == 0:
                     length = time.time_ns() - last_transition
-                    bit_length.append(length)
+                    bits_length.append(length)
                     state = STATE_BIT_DATA
                 elif time.time_ns() - last_transition > SERIAL_NO_MORE_DATA:
                     state = STATE_END
 
-        print(min(bit_length))
-        print(max(bit_length))
-        print(str(bit_length))
+        print(min(bits_length))
+        print(max(bits_length))
+        print(str(bits_length))
 
 
 def make_pigpio_transport():
@@ -106,3 +113,53 @@ if not pigpio:
 transport = make_pigpio_transport()
 transport.setup()
 transport.read()
+
+exit(1)
+
+class pigpio:
+    INPUT = 1
+    OUTPUT = 2
+    PUD_UP = 1
+    LOW = 0
+    HIGH = 1
+
+class FakePi:
+    def __init__(self):
+        self.records = []
+
+    def set_mode(self, pin, mode):
+        rec = (time.time_ns(), 'set_mode', pin, "input" if mode == pigpio.INPUT else "output")
+        self.records.append(rec)
+
+    def set_pull_up_down(self, pin, pud):
+        rec = (time.time_ns(), 'set_mode', pin, "pull up" if pud == pigpio.PUD_UP else "pull down")
+        self.records.append(rec)
+
+    def read(self, pin):
+        self.records.append((time.time_ns(), 'read', pin))
+        if len(self.records) > 100:
+            raise ValueError("Bad")
+
+        start = time.time_ns()
+        while time.time_ns() - start < 1000:
+            continue
+        return pigpio.LOW
+
+    def write(self, pin, value):
+        self.records.append((time.time_ns(), 'write', pin, value))
+
+    def log(self):
+        prev = self.records[0][0]
+        for rec in self.records:
+            print(rec, rec[0] - prev)
+            prev = rec[0]
+
+
+try:
+    pi = FakePi()
+    transport = SingleWireSerialTransport(pi, GPIO14)
+    transport.setup()
+    transport.read()
+finally:
+    pi.log()
+
