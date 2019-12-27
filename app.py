f1bb26fad49297d94fdf40e7e2cf7ef247e7ea85
Francois Picalausa <fpicalausa@gmail.com>
2019-12-27 17:17:44 +0900

    Fix truncate

--- app.py
+++ app.py
@@ -41,7 +41,8 @@ if os.path.exists(HISTFILE):
 def on_data(data):
     if os.path.getsize(HISTFILE) > 500000:
         with open(HISTFILE, 'r') as f:
-            previous = [json.loads(l) for l in f.readlines()][:5000]
+            previous = [json.loads(l) for l in f.readlines() if len(l.strip()) > 0]
+            previous = previous[:5000]
 
         with open(HISTFILE, 'w') as f:
             f.write("\n".join(previous))
